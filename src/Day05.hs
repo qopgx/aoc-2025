396b7f0c23a7463a5dddabb62bb6b36183291eb7
qopgx <>
2026-01-06 18:00:42 +0100

    Day05 solved
    Signed-off-by: qopgx <>

--- src/Day05.hs
+++ src/Day05.hs
@@ -1,10 +1,44 @@
 module Day05 where
 
+import Data.List
+import Data.List.Split
 import Paths_aoc2025 (getDataFileName)
+
+mergeRanges :: [Int] -> [[Int]] -> [[Int]]
+mergeRanges r [] = [r]
+mergeRanges r@[lo, hi] rs@([lo', hi'] : t)
+  | lo' - 1 <= hi = [lo, max hi' hi] : t
+  | otherwise = r : rs
+mergeRanges _ _ = error "mergeRanges: Impossible"
+
+inRange [l, r] x = l <= x && x <= r
+inRange _ _ = error "inRange: Impossible"
+
+orHigher predicate [l, r] x = predicate [l, r] x || r < x
+orHigher _ _ _ = error "Impossible"
+
+getMergedRanges ranges = foldr mergeRanges [] $ sortOn head $ sortOn last ranges
+
+solve :: [[Int]] -> [Int] -> [Int]
+solve ranges ingredients =
+  let mergedRanges = getMergedRanges ranges
+      sortedIngredients = sort ingredients
+   in concat $
+        snd $
+          mapAccumL
+            ( \a b ->
+                let trimmed =
+                      dropWhile (not . orHigher inRange b) a
+                 in (trimmed, takeWhile (inRange b) trimmed)
+            )
+            sortedIngredients
+            mergedRanges
 
 day05 :: IO ()
 day05 = do
-  inputLines <- lines <$> (getDataFileName "day05-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 05"
+  [inputRanges, inputIngredients] <- splitOn "\n\n" <$> (getDataFileName "day05-input.txt" >>= readFile)
+  let ranges = map (map (read :: String -> Int) . splitOn "-") $ lines inputRanges
+      ingredients = map (read :: String -> Int) $ lines inputIngredients
+  print $ length $ solve ranges ingredients
+  -- Second one didn't make a big difference
+  print $ foldl' (\a [l, r] -> r - l + 1 + a) 0 $ getMergedRanges ranges
